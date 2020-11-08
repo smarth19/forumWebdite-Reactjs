83a65dbe5915ad3cd8b9a37db1e849aa1c855cb0
Samarth <mittalsmarth19@gmail.com>
2020-11-08 12:59:18 +0530

    sixth commit

--- server.js
+++ server.js
@@ -691,10 +691,10 @@ app.get('/loadMoreSearchResults/:q?', async (req, res) => {
 //Serve Static file if in production
 if(process.env.NODE_ENV === 'production'){
     // set static folder
-    app.use(express.static('/client/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname + '/client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 // Making Server to listen on port 5000
